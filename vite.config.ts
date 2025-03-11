a80ea2b5b367c28c304ebf794fa5d5b56c9bb299
VoHoangKiet <muoivo.051064@gmail.com>
2025-03-11 20:26:23 +0700

    fix port

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: 'localhost',
-    port: 3000,
+    port: 3001,
   },
   resolve: {
     alias: {

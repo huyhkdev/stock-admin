69f3f5d1a814e5a95e9b7ecf09955b3ac76beb42
Kim huy Huỳnh <kimhuyhuynh@Kims-MacBook-Air.local>
2025-03-11 21:59:22 +0700

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/dashboard/',
   server: {
     host: 'localhost',
     port: 3001,
